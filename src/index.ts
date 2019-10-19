4d5a6056dee8b9a6004006aafd3af46ff97b5b22
Hans Christian Reinl <info@drublic.de>
2019-10-19 23:21:25 +0200

    Export correct files, also with builds

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,16 @@ export { default as validate, ValidatorTypes } from "./Form/validators";
 
 export { default as Grid } from "./Grid";
 export { default as GridItem } from "./GridItem";
+
 export { default as Icon, IconNames } from "./Icon";
 export { default as List } from "./List";
 export { default as ListCard } from "./ListCard";
 export { default as Listing } from "./Listing";
 export { default as Table } from "./Table";
 export { default as Typography } from "./Typography";
+
+export { Breakpoints } from "./Breakpoints";
+export { State } from "./Colors";
 
 export type Validator = Validator;
 export type Validators = Validators;
